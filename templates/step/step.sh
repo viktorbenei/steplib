799b76432e9efa1e1d3aad7470ceb530c260c88b
Viktor Benei <viktor.benei@gmail.com>
2014-10-20 16:06:52 +0200

    templates: a base step repo template with annotated step.sh and step.yml files

---
+++ templates/step/step.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#
+# This is the entry point of your Step.
+# You can work in this Bash file directly
+#  or if you can include another script / program in your step
+#  repository and call/run it from this script file. 
+#
+# Note: you should return a proper exit code. 0 is success, 1+ is error
+#
+# Don't forget to list your input and output environment variables
+#  in the step.yml file!
+#
+
+echo "Hello World"
+exit 0
